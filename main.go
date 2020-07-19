78b0024209184ee505790a9484cbc70b9e4b25df
Icaro-Lima <icarodantas.lima@gmail.com>
2020-07-18 21:21:11 -0300

    WIP: Changing database

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"github.com/joho/godotenv"
 	"github.com/learn-qsharp/learn-qsharp-api/db"
+	"github.com/learn-qsharp/learn-qsharp-api/github"
+	"github.com/learn-qsharp/learn-qsharp-api/tutorials"
 	"log"
+	"os"
 )
 
 func main() {
@@ -16,16 +19,18 @@ func main() {
 	}
 	defer dbc.Close(context.Background())
 
-	/*if os.Getenv("GITHUB_IGNORE") != "true" {
-		githubClient, githubCtx := github.Setup()
+	if os.Getenv("GITHUB_IGNORE") != "true" {
+		ctx := context.Background()
 
-		err = tutorials.LoadFromGithubAndSaveToDb(dbc, githubClient, githubCtx)
+		githubClient := github.Setup(ctx)
+
+		err = tutorials.LoadFromGithubAndSaveToDb(ctx, dbc, githubClient)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
-	err = router.Run(dbc)
+	/*err = router.Run(dbc)
 	if err != nil {
 		log.Fatal(err)
 	}*/
