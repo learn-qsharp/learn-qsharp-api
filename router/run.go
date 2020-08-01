a943ae5989fcfaec39f4529162203d3ac92f54a1
Icaro-Lima <icarodantas.lima@gmail.com>
2020-08-01 16:32:10 -0300

    Fix database pool + refactoring

--- router/run.go
+++ router/run.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/learn-qsharp/learn-qsharp-api/api"
 )
 
-func Run(db *pgx.Conn) error {
+func Run(db *pgxpool.Pool) error {
 	r := gin.Default()
 	r.Use(cors.Default())
 	r.Use(func(c *gin.Context) {
