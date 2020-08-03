3ffcb7ab2d1a9d2e6d2a336a216b70e770988206
Icaro-Lima <icarodantas.lima@gmail.com>
2020-08-02 21:09:57 -0300

    Minimal refactoring

--- db/setup.go
+++ db/setup.go
@@ -7,6 +7,14 @@ import (
 	"github.com/jackc/tern/migrate"
 )
 
+func SetupPgxConn(ctx context.Context, databaseURL string) (*pgx.Conn, error) {
+	return pgx.Connect(ctx, databaseURL)
+}
+
+func SetupPgxPool(ctx context.Context, databaseURL string) (*pgxpool.Pool, error) {
+	return pgxpool.Connect(ctx, databaseURL)
+}
+
 func Migrate(ctx context.Context, pgxConn *pgx.Conn) error {
 	m, err := migrate.NewMigrator(ctx, pgxConn, "schema_version")
 	if err != nil {
@@ -19,12 +27,4 @@ func Migrate(ctx context.Context, pgxConn *pgx.Conn) error {
 	}
 
 	return m.Migrate(ctx)
-}
-
-func SetupPgxConn(ctx context.Context, databaseURL string) (*pgx.Conn, error) {
-	return pgx.Connect(ctx, databaseURL)
-}
-
-func SetupPgxPool(ctx context.Context, databaseURL string) (*pgxpool.Pool, error) {
-	return pgxpool.Connect(ctx, databaseURL)
 }
